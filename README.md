79a6a5d097a9e2befe9f3f1cde2e06431f9bf145
Demeter Áron <demeter.aron0614@gmail.com>
2023-06-02 16:58:39 +0200

    yesnt

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # OC-ip
 A ipv4 like network system
+
+dev notes:
+protocol:
+PacketStructure:
+
+[TTL-byte][PROTOCOL-BYTE]:[SourceAddrLenByte][SAddress]:[DestAddrLenByte][DAddress]:[Data]
+
+Protocols:
+    0x00: ICMP
+    0x01: ICMP_PING
+    0x02: ICMP_PONG
+    
--- src/interface..lua
+++ src/interface..lua
@@ -12,7 +12,12 @@ local interface = {
     id = nil,
     name = nil,
     type = nil,
+
+    ---actuall stuff (component vars)
+    compID = nil,
+    proxy = nil,
 }
+interface.__index = interface
 
 --- the interfaces should include the following functions:
 --- new() - creates a new interface
@@ -21,4 +26,28 @@ local interface = {
 --- getInfo() - returns the interface info
 
 --- the modem interface
+local modemInterface = interface
 
+--- modemInter Constructor
+function modemInterface.new(proxy, name, id)
+    local self = setmetatable({}, modemInterface)
+    if component.type(proxy.address) ~= "modem" then
+        return
+    end
+
+    self.id = id
+    self.proxy = proxy
+    self.compID = proxy.address
+
+    self.name = name
+    self.type = "modem"
+
+    return self
+end
+
+--- modemInter send
+function modemInterface:send(packet)
+    self.proxy.send(packet)
+    self.packetsOut = self.packetsOut + 1
+    self.bytesOut = self.bytesOut + packet:len()
+end
